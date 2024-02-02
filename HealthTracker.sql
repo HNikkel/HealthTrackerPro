06624d6447cae75a8cd2aa5c3704df154cba18ca
AustinFortin <156240645+AustinFortin@users.noreply.github.com>
2024-02-01 20:12:36 -0800

    Updated HealthTracker.sql
    Fixed improper export of goals table

--- HealthTracker.sql
+++ HealthTracker.sql
@@ -1,81 +1,90 @@
--- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: HealthTracker
--- ------------------------------------------------------
--- Server version	8.0.35-0ubuntu0.22.04.1
+-- Host: localhost:3306
+-- Generation Time: Feb 01, 2024 at 08:10 PM
+-- Server version: 8.0.35-0ubuntu0.22.04.1
+-- PHP Version: 8.1.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `HealthTracker`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Goals`
 --
 
-DROP TABLE IF EXISTS `Goals`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Goals` (
-  `goal_id` int NOT NULL AUTO_INCREMENT,
+  `goal_id` int NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `creation_date` date DEFAULT NULL,
   `completion_date` date DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`goal_id`),
-  KEY `username` (`username`),
-  CONSTRAINT `Goals_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Users` (`Username`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `Goals`
---
-
-LOCK TABLES `Goals` WRITE;
-/*!40000 ALTER TABLE `Goals` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Goals` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Users`
 --
 
-DROP TABLE IF EXISTS `Users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Users` (
   `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  PRIMARY KEY (`Username`)
+  `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Users`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `Users` WRITE;
-/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES ('austin','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8'); 
-/*password is password*/;
-/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `Goals`
+--
+ALTER TABLE `Goals`
+  ADD PRIMARY KEY (`goal_id`),
+  ADD KEY `username` (`username`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Goals`
+--
+ALTER TABLE `Goals`
+  MODIFY `goal_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Goals`
+--
+ALTER TABLE `Goals`
+  ADD CONSTRAINT `Goals_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Users` (`Username`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-01-26 20:42:06
